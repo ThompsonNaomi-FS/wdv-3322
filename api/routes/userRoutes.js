29158d694a802bfb530cc818686bcd2ebd353825
Naomi Thompson <NAThompson1@student.fullsail.edu>
2022-10-30 16:51:25 -0500

    working on route functionality

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const { default: mongoose } = require("mongoose");
 const router = express.Router();
 const { findUser, saveUser } = require('../../db/db');
 
 router.post('/signup', (req,res) => {
-    findUser({ 
+    saveUser({ 
+        _id: mongoose.Types.ObjectId(),
         firstName: req.body.firstName, 
         lastName: req.body.lastName,
         address: req.body.address,
@@ -12,12 +14,27 @@ router.post('/signup', (req,res) => {
         zip: req.body.zip,
         email: req.body.email,
         password: req.body.password
-    }) 
-    res.status(201).json({ message: 'POST' })
+    })
+    .then(result => {
+        res.status(200).json({
+            message: "User Saved",
+            user: {
+                "ID": result._id,
+                "First Name": result.firstName,
+                "Last Name": result.lastName,
+                "Address": result.address,
+                "City": result.city,
+                "State": result.state,
+                "Zip": result.zip,
+                "Email": result.email,
+                "Password": result.password
+            }
+        })
+    })
 });
 
 router.post('/login', (req, res) => {
-
+    res.status(201).json({ message: 'LOGIN' });
 });
 
 router.get('/', (req, res) => {
@@ -26,7 +43,12 @@ router.get('/', (req, res) => {
 
 router.get('/profile/:id', (req, res) => {
     const id = req.params.id;
-    res.status(201).json({ message: 'GET by Id', id:id });
+
+    findUser(id)
+    .then(result => {
+        res.status(201).json({ message: 'GET by Id', id:id });
+        console.log(result);
+    })
 });
 
 router.patch('/:id', (req, res) => {
--- db/db.js
+++ db/db.js
@@ -1,9 +1,46 @@
-const findUser = async (object) => {
-    
+const users = [
+    {
+    _id: '23',
+    firstName: 'Naomi',
+    lastName: 'Thompson',
+    address: '255 N 5th St',
+    city: 'Ulysses',
+    state: 'NE',
+    zip: '68669',
+    email: 'nameowmi@outlook.com',
+    password: 'password'
+    },
+    {
+    _id: '24',
+    firstName: 'Odin',
+    lastName: 'Thompson',
+    address: '255 N 5th St',
+    city: 'Ulysses',
+    state: 'NE',
+    zip: '68669',
+    email: 'odin@outlook.com',
+    password: 'password'
+        }
+];
+
+const findUser = async (data) => {
+    // const match = users.filter(user => {
+    //     return user._id.includes(data)
+    // });
+    // const match = {}; 
+    for (let i = 0; i < users.length++; i++) {
+        if (users[i] === data) return users[i];
+    }
+    // console.log("From findUser: ")
+    // console.log(match);
+
 };
 
-const saveUser = async () => {
-
+const saveUser = async (data) => {
+    users.push(data)
+    console.log('From saveUser:')
+    console.log(users)
+    return data;
 };
 
 module.exports = {
