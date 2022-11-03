7f883dd8d2ab417550727cfe002d3a976a7dc8bf
Naomi Thompson <NAThompson1@student.fullsail.edu>
2022-11-03 17:12:59 -0500

    removed psuedo code

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -8,15 +8,12 @@ const bcrypt = require('bcrypt');
 const user = {};
 
 router.post('/signup', (req,res) => {
-    // findUser by email address { email: email }
     findUser({email: req.body.email})
-    // if user exists return 409 (conflict) with message about user existing
     .then(result => {
         if(result){
             res.status(409).json({
                 message: "That email address is already in use! Please try logging in."
             });
-    // else encrypt the password
         } else {
             bcrypt.hash(req.body.password, 10, (err, hash) => {
                 if(err){
@@ -24,8 +21,6 @@ router.post('/signup', (req,res) => {
                 } else {
                     user.password = hash;
                     res.status(200).json({ message: `User with email of ${req.body.email} created successfully!` });
-    // create a new user object with the hashed password as the new password
-    // saveUser (user.save(user))
                     const newUser = new User({
                         _id: mongoose.Types.ObjectId(),
                         firstName: req.body.firstName, 
@@ -45,18 +40,12 @@ router.post('/signup', (req,res) => {
 });
 
 router.post('/login', (req, res) => {
-    // findUser
     findUser({email: req.body.email})
-    // if user not found then return 401 (not authorized) with message saying authorization failed
     .then(result => {
         if(!result){
             res.status(401).json({
                 message: "That email address is not in our system! Please sign up and try again."
             });
-    // else compare passwords
-    // test for error in side callback function
-    // test the result (true or false)
-    // return message authorization successful
         } else {
             bcrypt.compare(req.body.password, user.password, (err, result) => {
                 if(err) return res.status(501).json({ message: err.message });
@@ -77,9 +66,7 @@ router.post('/login', (req, res) => {
 });
 
 router.get('/profile', (req, res) => {
-    const email = req.params.email;
-
-    findUser(email)
+    findUser({email: req.body.email})
     .then(result => {
         if(result){
             res.status(200).json({
